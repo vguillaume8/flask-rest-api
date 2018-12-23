20163c588db8d4c10c7d22141c33f2d797686e68
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-12-22 23:42:08 -0500

    API helper functions

--- endpoints.py
+++ endpoints.py
@@ -4,17 +4,51 @@ app = Flask(__name__)
 
 #Make an app.route() decorator here for when the client sends the URI "/puppies"
 
-@app.route('/puppies')
+@app.route('/puppies', methods = ['GET', 'POST'])
 def puppiesFunction():
-  return "Yes, puppies!"
+  if request.method == 'GET':
+    # Call the method to Get all of the puppies
+    return getAllPuppies()
+  
+  elif request.method == 'POST':
+    # Call the method to make a new puppy
+    return makeANewPuppy()
+
+
+
   
  
 #Make another app.route() decorator here that takes in an integer named 'id' for when the client visits a URI like "/puppies/5"
 
-@app.route('/puppies/<int:id>')
+@app.route('/puppies/<int:id>', methods = ['GET', 'PUT', 'DELETE'])
 def puppiesFunctionId(id):
-  return "This method will act on the puppy with id %s" % id
+  if request.method == 'GET:
+    # Call the method to get a specific puppy based on their id
+    return getPuppy(id)
 
+  if request.method == 'PUT':
+    # Call the method to update a puppy
+    return updatePuppy(id)
+
+  elif request.method == 'DELETE':
+    # Call the method to remove a puppy
+    return deletePuppy(id)
+
+def getAllPuppies():
+  return "Get All the puppies!"
+
+def makeANewPuppy():
+  return "Creating a New Puppy!"
+
+def getPuppy(id):
+  return "Getting Puppy with id %s" % id
+
+def updatePuppy(id):
+  return "Updating a Puppy with id %s" % id
+
+def deletePuppy(id):
+  return "Removing Puppy with id %s" % id
+  
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)	
